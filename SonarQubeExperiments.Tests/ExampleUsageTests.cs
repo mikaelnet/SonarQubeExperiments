6da7f164b4b8ae3ed7bca50fa1b447565e3aa50e
Mikael Högberg <mikael.hogberg@stendahls.se>
2022-06-17 10:11:47 +0200

    refactored projects

--- SonarQubeExperiments.Tests/ExampleUsageTests.cs
+++ SonarQubeExperiments.Tests/ExampleUsageTests.cs
@@ -5,37 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
-namespace SonarQubeExperiments
+namespace SonarQubeExperiments.Tests
 {
-    public interface IModelWrapper
-    {
-        Guid Id { get; }
-        string Name { get; set; }
-        IModelWrapper Parent { get; }
-
-        T GetAncestorOrSelf<T>() where T : IModelWrapper;
-    }
-
-    public abstract class ModelWrapperBase : IModelWrapper
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-
-        public IModelWrapper Parent { get; set; }
-
-        public virtual T GetAncestorOrSelf<T> () where T : IModelWrapper
-        {
-            if (this is T)
-                return (T)(this as IModelWrapper);
-
-            var parent = this.Parent;
-            if (parent == null)
-                return default(T);
-
-            return parent.GetAncestorOrSelf<T>();
-        }
-    }
-
     public class ModelX : ModelWrapperBase
     {
         public string XField { get; set; }
---
+++ SonarQubeExperiments/ModelWrapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarQubeExperiments
+{
+    public interface IModelWrapper
+    {
+        Guid Id { get; }
+        string Name { get; set; }
+        IModelWrapper Parent { get; }
+
+        T GetAncestorOrSelf<T>() where T : IModelWrapper;
+    }
+
+    public abstract class ModelWrapperBase : IModelWrapper
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public IModelWrapper Parent { get; set; }
+
+        public virtual T GetAncestorOrSelf<T> () where T : IModelWrapper
+        {
+            if (this is T)
+                return (T)(this as IModelWrapper);
+
+            var parent = this.Parent;
+            if (parent == null)
+                return default(T);
+
+            return parent.GetAncestorOrSelf<T>();
+        }
+    }
+
+    
+}
